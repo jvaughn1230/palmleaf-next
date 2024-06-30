0a7c38f33d9227ad915b718c2df650691073567c
Alex <jvaughn1230@gmail.com>
2024-06-29 23:44:20 -0700

    finish connecting redux for categories

---
+++ app/store/categories/category.selector.ts
@@ -0,0 +1,25 @@
+import { createSelector } from "reselect";
+import { RootState } from "../store";
+import { CategoryMapType } from "@/types";
+
+const selectCategorySlice = (state: RootState) => state.categories;
+
+export const SelectCategories = createSelector(
+  [selectCategorySlice],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [SelectCategories],
+  (categories): CategoryMapType =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {} as CategoryMapType)
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategorySlice],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
